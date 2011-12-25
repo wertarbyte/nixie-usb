6e927372eddeb77efc2b9a9d14e2646d06caa82e
Stefan Tomanek <stefan@pico.ruhr.de>
2011-12-25 20:24:28 +0100

    centralize communication with device

--- software/nixie.c
+++ software/nixie.c
@@ -36,6 +36,38 @@ uint8_t open_usb(usb_dev_handle **handle) {
 	}
 }
 
+static int send_buffer(usb_dev_handle *handle, uint8_t *buf, uint8_t l) {
+	int8_t sent = usb_control_msg(handle,
+			USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_OUT,
+			CUSTOM_RQ_SET_NIXIE,
+			0, 0,
+			buf, l,
+			100);
+	if (sent < l) {
+		fprintf(stderr, "Unable to send to USB nixie\n");
+		return 1;
+	}
+	return 0;
+}
+
+static int set_tube(usb_dev_handle *handle, uint8_t tube, uint8_t value) {
+	uint8_t buf[3];
+	buf[0] = CUSTOM_RQ_CONST_TUBE;
+	buf[1] = (uint8_t) tube;
+	buf[2] = (uint8_t) value;
+	return send_buffer(handle, buf, sizeof(buf));
+}
+
+static int set_led(usb_dev_handle *handle, uint8_t led, uint8_t r, uint8_t g, uint8_t b) {
+	uint8_t buf[5];
+	buf[0] = CUSTOM_RQ_CONST_LED;
+	buf[1] = (uint8_t) led;
+	buf[2] = (uint8_t) r;
+	buf[3] = (uint8_t) g;
+	buf[4] = (uint8_t) b;
+	return send_buffer(handle, buf, sizeof(buf));
+}
+
 int main(int argc, char *argv[]) {
 	usb_dev_handle *handle = NULL;
 	int usb_present = open_usb(&handle);
@@ -54,41 +86,18 @@ int main(int argc, char *argv[]) {
 		int b = 0;
 		if (sscanf(argv[0], "t%d:%d", &tube, &value) == 2 && tube >= 0 && value >= 0) {
 			printf("Setting nixie tube %u to %u.\n", tube, value);
-			uint8_t buf[3];
-			buf[0] = CUSTOM_RQ_CONST_TUBE;
-			buf[1] = (uint8_t) tube;
-			buf[2] = (uint8_t) value;
-			int8_t sent = usb_control_msg(handle,
-					USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_OUT,
-					CUSTOM_RQ_SET_NIXIE,
-					0, 0,
-					buf, sizeof(buf),
-					100);
-			if (sent < sizeof(buf)) {
+			if (set_tube(handle, tube, value)) {
 				fprintf(stderr, "Unable to send to USB nixie\n");
 				usb_close(handle);
 				return 1;
 			}
 		} else if (sscanf(argv[0], "l%d:%d/%d/%d", &tube, &r, &g, &b) == 4 && tube >= 0) {
 			printf("Setting nixie LED %u to %u/%u/%u.\n", tube, r, g, b);
-			uint8_t buf[5];
-			buf[0] = CUSTOM_RQ_CONST_LED;
-			buf[1] = (uint8_t) tube;
-			buf[2] = (uint8_t) r;
-			buf[3] = (uint8_t) g;
-			buf[4] = (uint8_t) b;
-			int8_t sent = usb_control_msg(handle,
-					USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_OUT,
-					CUSTOM_RQ_SET_NIXIE,
-					0, 0,
-					buf, sizeof(buf),
-					100);
-			if (sent < sizeof(buf)) {
+			if (set_led(handle, tube, r, g, b)) {
 				fprintf(stderr, "Unable to send to USB nixie\n");
 				usb_close(handle);
 				return 1;
 			}
-
 		} else {
 			fprintf(stderr, "Unable to parse command line item: %s.\n", argv[0]);
 			usb_close(handle);
